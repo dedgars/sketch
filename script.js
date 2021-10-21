540ecba2635739c7a2b2a5c10e010efe6d6336d3
Edgars <duminsh@gmail.com>
2021-10-21 05:44:57 +0300

    total remake

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
 
-let size = '320px'
-const containerDiv = document.querySelector('#container');
-containerDiv.style.height = size;
-containerDiv.style.width = size;
-containerDiv.style.backgroundColor = "green";
 
+const container = document.querySelector('#container');
 
+function createPad() {
+    const sketchPad = document.createElement("DIV");
+    sketchPad.setAttribute('id', 'sketchPadDiv')
+    container.appendChild(sketchPad);
 
+    for (let i = 0; i < 64; i++ ) {
+        let pixel = document.createElement("DIV");
+        pixel.setAttribute('id', 'pixel');
 
+        pixel.addEventListener('mouseenter', function( event ) {
+            event.target.style.backgroundColor = "lightgreen";
+            console.log('hee')
+        }, false);
 
+        sketchPad.appendChild(pixel)};
 
-for (let i = 0; i < 8 * 8; i++){
-    const pixel = document.createElement("div");
-    pixel.style.height = '40px';
-    pixel.style.width = '40px';
-    pixel.style.backgroundColor = "lightGreen";
-    pixel.style.display = "inline-block";
-    pixel.style.verticalAlign = "top";
-    
-    pixel.addEventListener('mouseenter', function( event ) {
-        event.target.style.backgroundColor = "grey";
-        // setTimeout(function() {
-        //     event.target.style.backgroundColor = "lightGreen";
-        // }, 700);
-    }, false);
+};
 
-    containerDiv.appendChild(pixel);
+function reset() {
+    const sketchPad = document.getElementById("sketchPadDiv");
+    sketchPad.remove();
+    createPad()
+};
 
-}
-
-
+createPad()
 
